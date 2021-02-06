af5991390559fea4525927ad1d898366ef9dfab5
Steve-P42 <78639312+Steve-P42@users.noreply.github.com>
2021-02-06 18:27:37 +0100

    added file

---
+++ greetings.py
@@ -0,0 +1,22 @@
+#   Welcome to this code, I hope you'll enjoy it
+# author: Steve
+# creation date:
+# purpose:
+# status:
+# %%
+print("Greetings, my Lord!")
+# %%
+
+# %%
+
+# %%
+
+# %%
+
+# %%
+
+# %%
+
+# %%
+
+# %%
