2ee7e2feb88a3dfa0d713cd0aa1154deef4b0fa9
SteveP42 <19IMC10917@fh-krems.ac.at>
2021-02-06 17:41:42 +0100

    Added venv and first Python file

---
+++ hello.py
@@ -0,0 +1,24 @@
+#   Welcome to this code, I hope you'll enjoy it
+# author: Steve-P42
+# creation date: 2021-02-06 16:39:29
+# purpose: Learning Git(Hub)
+# status: in progress
+# %%
+from time import gmtime, strftime
+
+print(strftime("%Y-%m-%d %H:%M:%S", gmtime()))
+# %%
+
+# %%
+
+# %%
+
+# %%
+
+# %%
+
+# %%
+
+# %%
+
+# %%
